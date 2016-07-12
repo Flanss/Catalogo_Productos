646b365e99cb68d27188d6c94862a3a3a4ce4787
Baltazar <calvillo.escalera@gmail.com>
2016-07-12 13:00:28 -0500

    crud for marcas

--- app/controllers/marcas_controller.rb
+++ app/controllers/marcas_controller.rb
@@ -6,16 +6,37 @@ class MarcasController < ApplicationController
 		@marca = Marca.find(params[:id])
 	end
 	def new
+		@marca = Marca.new
+	end
+	def edit
+	  @marca = Marca.find(params[:id])
 	end
 	def create
 		@marca = Marca.new(marca_params)
-		@marca.save
+		if @marca.save
 		redirect_to action: "index"
+		else render 'new'
+		end
 	end
+	def update
+	  @marca = Marca.find(params[:id])
+	 
+	  if @marca.update(marca_params)
+	    redirect_to action: "index"
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	    @marca = Marca.find(params[:id])
+	    @marca.destroy
+	    redirect_to action: "index"
+ 	 end
+
 
 	private
 	  def marca_params
 	    params.require(:marca).permit(:nombre, :text)
 	  end
-end
-
+	end
